b993d3cfec3d1089ddb7813f4e07f43cb08d0629
lamiru <lamiru@gmail.com>
2018-07-30 23:01:47 +0900

    removing the local state

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,6 +11,14 @@ class Counters extends Component {
     ]
   }
 
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  }
+
   handleReset = () => {
     const counters = this.state.counters.map(c => {
       c.value = 0;
@@ -29,7 +37,12 @@ class Counters extends Component {
       <div>
         <button onClick={this.handleReset} className='btn btn-primary btn-sm m-2'>Reset</button>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} onDelete={this.handleDelete} counter={counter}></Counter>
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            counter={counter}>
+          </Counter>
         ))}
       </div>
     );
